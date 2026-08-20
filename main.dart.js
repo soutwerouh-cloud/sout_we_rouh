f1be174f61ac7f16298d10ace4d51be8bda66672
soutwerouh-cloud <soutwerouh-cloud@users.noreply.github.com>
2026-08-20 09:05:59 +0000

    deploy: 35457543764362dd5a8b8f1bd76e6a0db405906e

--- main.dart.js
+++ main.dart.js
@@ -29839,7 +29839,7 @@ $S:2}
 A.aC9.prototype={
 $1(a){var s=A.dg().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/5f77625673248ee5846fbcaf5d3e1a3878386fd7/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/5d531788691ec3404cac0cee66ead4007b177363/":s)+a},
 $S:85}
 A.Pz.prototype={
 gu(a){var s=this.a
