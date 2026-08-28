507b386a9f6f7af23e641032650feecefb7137e9
Mai Mahmoud <soutwerouh@gmail.com>
2026-08-28 22:03:57 +0300

    update compileSdk to 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.sout_we_rouh"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
