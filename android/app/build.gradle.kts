2918d8bdc6e7adb8a4551028c9e6084c90526e27
Mai Mahmoud <soutwerouh@gmail.com>
2026-08-29 18:19:15 +0300

    add missingDimensionStrategy for file_picker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        missingDimensionStrategy("content-type", "text")
     }
 
     buildTypes {
