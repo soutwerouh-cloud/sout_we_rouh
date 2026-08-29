bb2965bf6ac8d978c41a7ab8f6d91eb7e001e4ac
Mai Mahmoud <soutwerouh@gmail.com>
2026-08-29 19:21:33 +0300

    update package name with underscore to match firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.soutwerouh"
+    namespace = "com.example.sout_we_rouh"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.soutwerouh"
+        applicationId = "com.example.sout_we_rouh"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = flutter.versionCode
