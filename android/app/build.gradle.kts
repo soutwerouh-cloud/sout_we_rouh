944bc84d2ae3deb4a0de3b5d3d2d04dd3868416d
Mai Mahmoud <soutwerouh@gmail.com>
2026-08-29 18:35:05 +0300

    fix file_picker and sdk version compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.sout_we_rouh"
-    compileSdk = 36
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 36
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         missingDimensionStrategy("content-type", "text")
